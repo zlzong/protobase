fdd90a9918b512ce4ba00129a7b75e835c23f2cb
zlzong <zlzong@outlook.com>
2024-09-20 22:49:02 +0800

    fix: 添加tcpClient demo

---
+++ src/test/TcpClientTest.cpp
@@ -0,0 +1,36 @@
+#include "net/EventLoopThread.h"
+#include "net/InetAddress.h"
+#include "net/TcpClient.h"
+#include "net/EventLoop.h"
+
+
+EventLoopThread eventLoopThread;
+int main() {
+    EventLoop *pLoop = eventLoopThread.startLoop();
+    InetAddress inetAddress(9999, "127.0.0.1");
+    TcpClient client(pLoop,inetAddress,"test");
+
+    client.setMessageCallback([](const ConnectionPtr &connection, Buffer *message, Timestamp timestamp) {
+        std::string receiveMessage = message->retrieveAllString();
+        LOG_INFO("time: {}, mesasge: {}", timestamp.localeString(), receiveMessage);
+        connection->send(receiveMessage);
+    });
+
+    client.setConnectionCallback([](const ConnectionPtr & connection) {
+        connection->send("123", 3);
+    });
+
+    client.enableRetry();
+    client.clientConnect();
+
+    int timerId = pLoop->runEvery(3*1000,[]{
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        LOG_ERROR("i do it");
+    });
+
+    std::this_thread::sleep_for(std::chrono::seconds(20));
+
+    pLoop->cancel(timerId);
+
+    std::this_thread::sleep_for(std::chrono::seconds(100));
+}
