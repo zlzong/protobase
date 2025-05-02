727c1f9b7c135b686d66afba3e4b55f87e9dc71a
zlzong <zlzong@outlook.com>
2025-05-02 22:22:53 +0800

    feat:remove decoder

--- src/net/Connection.cpp
+++ src/net/Connection.cpp
@@ -144,23 +144,7 @@ void Connection::onRead(Timestamp receiveTime) {
     size_t nRead = m_inputBuffer.readFd(fd());
     if (nRead > 0) {
         if (m_messageCallback) {
-            if (m_decoder) {
-                while (m_inputBuffer.readableBytes() != 0) {
-                    try {
-                        Buffer decodedBuff = m_decoder->decode(&m_inputBuffer);
-                        if (decodedBuff.readableBytes() == 0) {
-                            break;
-                        }
-                        m_messageCallback(std::static_pointer_cast<Connection>(shared_from_this()), &decodedBuff, receiveTime);
-                    } catch (std::exception& e) {
-                        LOG_ERROR(e.what());
-                        forceClose();
-                        break;
-                    }
-                }
-            } else {
-                m_messageCallback(std::static_pointer_cast<Connection>(shared_from_this()), &m_inputBuffer, receiveTime);
-            }
+            m_messageCallback(std::static_pointer_cast<Connection>(shared_from_this()), &m_inputBuffer, receiveTime);
         }
     } else if (nRead == 0) {
         LOG_WARN("connection reset by peer");
